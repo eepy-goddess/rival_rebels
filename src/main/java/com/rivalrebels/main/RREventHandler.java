95714793439ee0cdf443eba2bf65e437acc1dc05
a-weeb-programmer <austinortega@outlook.com>
2022-01-19 12:25:22 -0700

    1.18.1 port begins

---
+++ src/main/java/com/rivalrebels/main/RREventHandler.java
@@ -0,0 +1,12 @@
+package com.rivalrebels.main;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = "rivalrebels", bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RREventHandler {
+
+}
---
+++ src/main/java/com/rivalrebels/main/RivalRebels.java
@@ -0,0 +1,10 @@
+package com.rivalrebels.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("rivalrebels")
+public class RivalRebels {
+    public RivalRebels(){
+
+    }
+}
